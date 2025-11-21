60b1c79cc642c8b52495c236966d72c1158368d2
marcoTheInsane <bimarcos1888@gmail.com>
2025-11-21 03:07:02 -0300

    Configurar WhiteNoise para servir estáticos en Railway

--- hotel_Lillo/settings.py
+++ hotel_Lillo/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',# Middleware para servir archivos estaticos en produccion
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -103,6 +104,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
